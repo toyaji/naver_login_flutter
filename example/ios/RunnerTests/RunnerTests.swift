aa12b6d69d93e534d0ec89f51bfc8061c0fab786
toyaji <happytoday83@naver.com>
2026-07-06 15:12:37 +0900

    refactor: remove unused imports in example iOS RunnerTests

--- example/ios/RunnerTests/RunnerTests.swift
+++ example/ios/RunnerTests/RunnerTests.swift
@@ -1,8 +1,4 @@
-import Flutter
-import UIKit
 import XCTest
-
-@testable import naver_login_flutter
 
 class RunnerTests: XCTestCase {
   // TODO: Add unit tests for FlutterNaverLoginPlugin if needed
